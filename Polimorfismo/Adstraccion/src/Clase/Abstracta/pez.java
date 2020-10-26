1b57aa23295c924d86975dcb8e71359073913b31
jefersson <jeferssoncamacho1940a@gmail.com>
2020-10-26 17:30:21 -0600

    Programacion

---
+++ Polimorfismo/Adstraccion/src/Clase/Abstracta/pez.java
@@ -0,0 +1,15 @@
+package Clase.Abstracta;
+
+public class pez extends SuperclaseAnimal {
+
+    public pez() {
+    }
+
+    @Override
+    public void alimentar() {
+
+        System.out.println("me alimento de yerbas y entre otras especias ");
+
+    }
+
+}
